bc19cef7e06236a1e50dd1c52fb000abad921d88
BakytDzhumabaev <djumabaevb@gmail.com>
2021-12-11 16:51:53 +0600

    trying to run

--- lib/pages/xcore.dart
+++ lib/pages/xcore.dart
@@ -1,3 +1,11 @@
-export 'main_view.dart';
-export 'main_controller.dart';
+// Main Page
 export 'main_binding.dart';
+export 'main_controller.dart';
+export 'main_view.dart';
+
+// Showroom Page
+export 'home/home_controller.dart';
+export 'home/home_view.dart';
+
+// Upcoming Page
+export 'pro_version/pro_version_view.dart';
